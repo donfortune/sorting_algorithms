1b392cac2fb55e9f1c1a79430b0598f4f948415f
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2024-01-20 16:07:51 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,6 +16,9 @@ void bubble_sort(int *array, size_t size)
 	size_t j;
 	int temp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1 - i; j++)
