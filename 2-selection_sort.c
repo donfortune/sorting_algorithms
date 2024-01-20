828aeecefe0e0d608d52ea297ca502fa7455cb2d
under20techie <133390683+under20techie@users.noreply.github.com>
2024-01-20 23:06:26 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,36 +3,35 @@
 
 /**
  * selection_sort - sorts an array of integers in ascending order
- * using selection_sort algorithm 
- * @array: The array of integer
+ * using selection_sort algorithm
+ * @array: The array of integers
  * @size: The size of the array
  *
  * Return: void
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+    size_t i, j, min;
+    int temp;
 
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	/**
-	* j moves size - 1 times 
-	* and i moves half of size times
-        */
-	for (j = 0; j < size - 1; j++)
-	{
-		/* Updates i to be in front of j by 1 */
-		for (i = j + 1; i < size; i++)
-		{
-			if (array[j] > array[i])
-			{
-				temp = array[j];
-				array[j] = array[i];
-				array[i] = temp;
-				print_array(array, size);
-			}
-		}
-	}
+    for (j = 0; j < size - 1; j++)
+    {
+        min = j;
+        for (i = j + 1; i < size; i++)
+        {
+            if (array[i] < array[min])
+                min = i;
+        }
+
+        if (min != j)
+        {
+            temp = array[j];
+            array[j] = array[min];
+            array[min] = temp;
+            print_array(array, size);
+        }
+    }
 }
