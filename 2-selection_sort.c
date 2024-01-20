77063d5d9423640bd6a8508faf54623d99e8ade2
under20techie <133390683+under20techie@users.noreply.github.com>
2024-01-20 23:18:33 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,26 +11,26 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
-	int temp;
+        size_t i, j, min;
+        int temp;
 
-	if (array == NULL || size < 2)
-		return;
+        if (array == NULL || size < 2)
+                return;
 
-	for (j = 0; j < size - 1; j++)
-	{
-		min = j;
-		for (i = j + 1; i < size; i++)
-		{
-			if (array[i] < array[min])
-				min = i;
-		}
-		if (min != j)
-		{
-			temp = array[j];
-			array[j] = array[min];
-			array[min] = temp;
-			print_array(array, size);
-		}
-	}
+        for (j = 0; j < size - 1; j++)
+        {
+                min = j;
+                for (i = j + 1; i < size; i++)
+                {
+                        if (array[i] < array[min])
+                                min = i;
+                }
+                if (min != j)
+                {
+                        temp = array[j];
+                        array[j] = array[min];
+                        array[min] = temp;
+                        print_array(array, size);
+                }
+        }
 }
