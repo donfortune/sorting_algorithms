c3b15146c5940cbf555bfec074112ec1ec8b7772
donfortune <donfortunet.df@gmail.com>
2024-01-22 12:47:00 -0800

    added files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,48 +2,46 @@
 #include <stdio.h>
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       in ascending order using the insertion sort algorithm.
- *
- * @list: A pointer to the head of the doubly linked list.
- *
- * Description: The function sorts the elements of the doubly linked list
- * in ascending order using the insertion sort algorithm.
+ * insertion_sort_list - sorts a doubly linked list
+ * @list: doubly linked list
  */
+
+
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *current, *temp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    current = *list;
+	current = *list;
 
-    while (current)
-    {
-        while (current->next && current->n > current->next->n)
-        {
-            temp = current->next;
-            current->next = temp->next;
-            temp->prev = current->prev;
+	while (current)
+	{
+		while (current->next && current->n > current->next->n)
+		{
+			temp = current->next;
+			current->next = temp->next;
+			temp->prev = current->prev;
 
-            if (current->prev)
-                current->prev->next = temp;
+			if (current->prev)
+				current->prev->next = temp;
 
-            if (temp->next)
-                temp->next->prev = current;
+			if (temp->next)
+				temp->next->prev = current;
 
-            current->prev = temp;
-            temp->next = current;
+			current->prev = temp;
+			temp->next = current;
 
-            if (temp->prev)
-                current = temp->prev;
-            else
-                *list = temp;
+			if (temp->prev)
+				current = temp->prev;
+			else
+				*list = temp;
 
-            print_list(*list);
-        }
+			print_list(*list);
+		}
 
-        current = current->next;
-    }
+		current = current->next;
+	}
 }
+
