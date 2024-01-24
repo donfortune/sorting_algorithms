8ee62d5e3b650f667fdd7e96fe3e8126231358fa
Mary Agiamah <maryagiamah@gmail.com>
2024-01-24 03:11:32 -0800

    TASK 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,47 +11,74 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t pivot_index = 0;
-	if (size > 1)
+
+	if (size > 1 && array)
 	{
-		pivot_index = lomuto_partition(array, 0, size - 1);
-
-		if (pivot_index != 0)
-			quick_sort(array, pivot_index);
-
-		if (pivot_index != size - 1)
-			quick_sort(array + pivot_index + 1, size - pivot_index - 1);
+		qs(array, 0, size - 1, size);
 	}
 }
 
-int lomuto_partition(int *array, size_t low, size_t high)
+
+/**
+ * qs - sorts an array of integers in ascending order
+ * using selection_sort algorithm
+ * @array: The array of integers
+ * @size: The size of the array
+ * @low: start
+ * @high: end
+ *
+ * Return: void
+ */
+void qs(int *array, size_t low, size_t high, size_t size)
+{
+	size_t pivot_index = 0;
+
+	if (low < high)
+	{
+		pivot_index = lomuto_partition(array, low, high, size);
+
+		if (pivot_index > low)
+			qs(array, low, pivot_index - 1, size);
+		if (pivot_index < high)
+			qs(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * lomuto_partition - sorts an array of integers in ascending order
+ * using selection_sort algorithm
+ * @array: The array of integers
+ * @size: The size of the array
+ * @low: start
+ * @high: end
+ * Return: void
+ */
+int lomuto_partition(int *array, size_t low, size_t high, size_t size)
 {
 	int pivot = array[high];
-	size_t n = array_size(array);
 	size_t i = low, temp = 0, j = 0;
 
-	for (j = 0; j < high; j++)
+	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			print_array(array, n);
+			if (array[j] != array[i])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+
+			}
 			i++;
 		}
 	}
-	temp = array[i];
-	array[i] = array[high];
-	array[high] = temp;
-
+	if (array[i] != array[high])
+	{
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
 	return (i);
-}
-
-size_t array_size(int *array)
-{
-	int j = 0;
-	for (; array[j]; j++)
-		;
-	return (j);
 }
